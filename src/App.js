16a503a81ff866aba828078057475e41c66118de
tanlenon <tanlenon@gmail.com>
2020-11-12 13:36:11 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -7,6 +7,29 @@ import React, { Component } from 'react';
 import ReactPaginate from 'react-paginate';
 import 'bootstrap/dist/css/bootstrap.css';
 
+const testData1 = [];
+const testData2 = [
+  {
+    "publish_date": "2016-09-05T23:25:47.642350Z",
+    "id": "9783221620868",
+    "body": "The fool doth think he is wise, but the wise man knows himself to be a fool.",
+    "author": "Kaley Schiller"
+  },
+  {
+    "rating": 3.2,
+    "id": "9793364045824",
+    "body": "Can one desire too much of a good thing?.",
+    "author": "Fay Lemke"
+  },
+  {
+    "rating": 4.1,
+    "publish_date": "2016-09-03T23:25:47.642545Z",
+    "id": "9784620626604",
+    "body": "How bitter a thing it is to look into happiness through another man's eyes!",
+  }
+];
+const testData3 = null;
+
 const months = [
   'Jan',
   'Feb',
@@ -32,14 +55,20 @@ const days = [
   'Sat'
 ];
 
+function convertToHTML(reviews) {
+  return reviews.slice(0, 20).map(rev => (
+    <Row key={rev.id} review={rev}/>
+  ));
+}
+
 class Stars extends Component {
 
   lastStar(amt) {
-    if (amt > 0.1 && amt <= .35) {
+    if (amt >= 0.1 && amt < .35) {
       return <img className="star star-1" key={this.props.id + '-last'} src={star_1} alt="star"/>
-    } else if (amt > 0.35 && amt <= .6) {
+    } else if (amt >= 0.35 && amt < .6) {
       return<img className="star star-2" key={this.props.id + '-last'} src={star_2} alt="star1"/>
-    } else if (amt > 0.6 && amt <= .9) {
+    } else if (amt >= 0.6 && amt <= .9) {
       return <img className="star star-3" key={this.props.id + '-last'} src={star_3} alt="star2"/>
     } else if (amt > 0.9) {
       return <img className="star" key={this.props.id + '-last'} src={star} alt="star3"/>
@@ -57,7 +86,7 @@ class Stars extends Component {
       star_images.push(<img className="star" key={this.props.id + '-' + i} src={star} alt="star"/>)
     }
 
-    if (percent > 0.1) {
+    if (percent >= 0.1) {
       star_images.push(this.lastStar(percent));
     }
 
@@ -65,6 +94,14 @@ class Stars extends Component {
       <div className="star-container">
         {star_images}
       </div>
+    )
+  }
+}
+
+class SortButton extends Component {
+  render() {
+    return (
+      <button className="sort-button" onClick={this.props.func}>{this.props.label}</button>
     )
   }
 }
@@ -77,12 +114,12 @@ class Row extends Component {
   }
   render() {
     let rev = this.props.review;
-    var d = new Date(rev.publish_date);
+    var d = rev.publish_date ? new Date(rev.publish_date) : '-';
     return (
       <div className="row-content">
-        <div className="rating"><span className="rating-text">{rev.rating}</span> <Stars stars={rev.rating} key={rev.id}/></div>
-        <div>{rev.author}</div>
-        <div>{days[d.getDay()] + ' - ' + months[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear()}</div>
+        <div className="review-box">{rev.rating ? <div className="rating"><span className="rating-text">{rev.rating}</span> <Stars stars={rev.rating} key={rev.id}/></div> : '-'}</div>
+        <div>{rev.author ? rev.author : '-'}</div>
+        <div className="review-date">{d !== '-' ? (days[d.getDay()] + ' - ' + months[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear()) : d}</div>
       </div>
     )
   }
@@ -100,7 +137,9 @@ class Container extends Component {
       offset: 0,
       perPage: 20,
       currentPage: 0,
-      average: 0,
+      average: '-',
+      highest: '-',
+      lowest: '-',
     };
   }
 
@@ -109,19 +148,28 @@ class Container extends Component {
       .then(res => res.json())
       .then(
         (result) => {
+
+          // *********************************** TEST 1 ***********************************
+          // result = testData1
+
+          // *********************************** TEST 2 ***********************************
+          // result = testData2
+
+          // *********************************** TEST 3 ***********************************
+          // result = testData3
+
+          result = result ? result : [];
           let count = result.length ? result.length : 0;
-          console.log(count,result);
+          let cleanResult = result.filter(a => a.rating);
           this.setState({
             isLoaded: true,
             reviews: result,
-            number_of_reviews: count,
-            reviews_on_page: count,
             pageCount: count ? Math.ceil(count / this.state.perPage) :  0,
-            average: Math.round((result.reduce((a,b) => a + b.rating, 0) / result.length) * 100) / 100,
+            average: cleanResult.length ? Math.round((cleanResult.reduce((a,b) => a + b.rating, 0) / cleanResult.length) * 100) / 100 : '-',
+            highest: cleanResult.length ? Math.max.apply(Math, cleanResult.map(function(o) { return o.rating; })) : '-',
+            lowest: cleanResult.length ? Math.min.apply(Math, cleanResult.map(function(o) { return o.rating; })) : '-'
           });
-          var rows = result.slice(0, 20).map(rev => (
-            <Row key={rev.id} review={rev}/>
-          ));
+          var rows = convertToHTML(result);
           this.setState({ thisPageReviews: rows });
         },
         // Note: it's important to handle errors here
@@ -136,6 +184,24 @@ class Container extends Component {
       )
   }
 
+  sortReviews(what, arr) {
+    if (what === 'rating lh') {
+      arr = arr.sort((a, b) => (a.rating > b.rating) ? 1 : -1)
+    } else if (what === 'rating hl') {
+      arr = arr.sort((a, b) => (a.rating > b.rating) ? -1 : 1)
+    } else if (what === 'date lh') {
+      arr = arr.sort((a, b) => (a.publish_date > b.publish_date) ? 1 : -1)
+    } else if (what === 'date hl') {
+      arr = arr.sort((a, b) => (a.publish_date > b.publish_date) ? -1 : 1)
+    } 
+
+    this.setState({
+      thisPageReviews: convertToHTML(arr),
+      offset: 0,
+      currentPage: 0,
+    });
+  }
+
   handlePageClick = (data) => {
     const selectedPage = data.selected;
 
@@ -146,10 +212,7 @@ class Container extends Component {
   }
 
   setElementsForCurrentPage() {
-    var rows = this.state.reviews.slice(this.state.offset, this.state.offset + this.state.perPage).map(rev => (
-      <Row key={rev.id} review={rev}/>
-    ));
-    console.log(rows, this.state.reviews)
+    var rows = convertToHTML(this.state.reviews.slice(this.state.offset, this.state.offset + this.state.perPage)); 
 
     this.setState({
       thisPageReviews: rows,
@@ -188,15 +251,36 @@ class Container extends Component {
               <span className="text-spacer">{this.state.average}</span>
               <Stars stars={this.state.average}/>
             </div>
-            <div className="state-box"><p className="large-header">Average Rating</p> {this.state.average}</div>
-            <div className="state-box"><p className="large-header">Average Rating</p> {this.state.average}</div>
+            <div className="state-box">
+              <p className="large-header">Highest Rating</p>
+              <span className="text-spacer">{this.state.highest}</span>
+              <Stars stars={this.state.highest}/>
+            </div>
+            <div className="state-box">
+              <p className="large-header">Lowest Rating</p>
+              <span className="text-spacer">{this.state.lowest}</span>
+              <Stars stars={this.state.lowest}/>
+            </div>
           </div>
           <div className="top-pagination-container">
             <div className="top-pagination">{paginationElement}</div>
           </div>
-          <div className="row-container-header">Shakepeare's Reviews ({(this.state.offset + 1) + ' - ' + (this.state.offset + this.state.thisPageReviews.length)})</div>
+          <div className="row-container-header">
+            Shakepeare's Reviews ({(this.state.reviews.length ? ((this.state.offset + 1) + ' - ') : '') + (this.state.offset + this.state.thisPageReviews.length)})
+            <div className="sort-buttons">
+              <p className="button-label-text">Sort by: </p>
+              <SortButton label={'rating high - low'} func={this.sortReviews.bind(this, 'rating hl', this.state.reviews)}/>
+              <SortButton label={'rating low - high'} func={this.sortReviews.bind(this, 'rating lh', this.state.reviews)}/>
+              <SortButton label={'date new - old'} func={this.sortReviews.bind(this, 'date hl', this.state.reviews)}/>
+              <SortButton label={'date old - new'} func={this.sortReviews.bind(this, 'date lh', this.state.reviews)}/>
+            </div>
+          </div>
           <div className="row-conatiner"> 
-          {this.state.thisPageReviews}
+          {this.state.thisPageReviews.length ? this.state.thisPageReviews : (
+            <div className="row-content">
+              <div className="review-date"><b>{'No results'}</b></div>
+            </div>
+          )}
           </div>
           <div className="row-container-end"></div>
         </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Container from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<Container />);
+  const reviewsElement = screen.getByText(/Shakepeare's Reviews/i);
+  expect(reviewsElement).toBeInTheDocument();
 });
